50e876f89f3328f3167caf614225d57e48a1744d
Tommy Boi <tomaarongrabowski@gmail.com>
2024-03-05 19:04:02 -0500

    Updated YouTube link to TC73 - TC 78 overview video

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export async function GET() {
-  const youtubeVideoUrl = "https://www.youtube.com/watch?v=3G_uCbKoG5A"; // Replace with actual YouTube video URL
+  const youtubeVideoUrl = "https://www.youtube.com/watch?v=Fyvit9gG8Yo"; // Replace with actual YouTube video URL
   redirect(youtubeVideoUrl);
 }
