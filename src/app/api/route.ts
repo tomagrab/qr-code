247cdaf3d7345e1b01c0f4257a85ebe4d9dc2f82
Tommy Boi <tomaarongrabowski@gmail.com>
2024-03-05 19:01:47 -0500

    Updated API route in QRCode

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export async function GET() {
-  const youtubeVideoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; // Replace with actual YouTube video URL
+  const youtubeVideoUrl = "https://www.youtube.com/watch?v=3G_uCbKoG5A"; // Replace with actual YouTube video URL
   redirect(youtubeVideoUrl);
 }
