6c157cc5ea0f03faa313e3055983bfc219714127
Tommy Boi <tomaarongrabowski@gmail.com>
2024-03-06 17:28:20 -0500

    Fixed bug with iFrame video where fullscreen was disallowed

--- src/components/EmbededYouTubeVideo/EmbeddedYouTubeVideo.tsx
+++ src/components/EmbededYouTubeVideo/EmbeddedYouTubeVideo.tsx
@@ -61,6 +61,7 @@ export default async function EmbeddedYouTubeVideo({
           src={embedUrl}
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
         ></iframe>
       </div>
     </div>
