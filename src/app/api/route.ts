5e174015be23d58e9aec52b6eb9c52caa14efefb
Tommy Boi <tomaarongrabowski@gmail.com>
2024-03-05 22:10:31 -0500

    Adjusted server action

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,11 +3,11 @@ import { redirect } from 'next/navigation';
 import { QRCodeURL } from '../../../db/schema';
 
 export async function GET() {
-  const youtubeVideoUrl: QRCodeURL | string =
-    (await getCurrentQRCodeURL()) ||
-    'https://www.youtube.com/watch?v=Fyvit9gG8Yo';
+  const youtubeVideoUrl: string | null = await getCurrentQRCodeURL();
 
-  console.log(youtubeVideoUrl);
+  if (youtubeVideoUrl === null) {
+    return redirect('/api');
+  }
 
   redirect(youtubeVideoUrl);
 }
