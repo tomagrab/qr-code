368c691a55019186b23fd075c5278ffea6d60c2e
Tommy Grabowski <tommy.grabowski@velocitor.com>
2024-03-06 10:37:03 -0500

    Updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,12 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ['/anyone-can-visit-this-route'],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ['/no-auth-in-this-route'],
+  // Routes that can be accessed while signed out, including the API routes
+  publicRoutes: ['/api'],
 });
 
 export const config = {
-  // Protects all routes, including api/trpc.
+  // Protects all routes, including api/trpc, except those explicitly made public.
   // See https://clerk.com/docs/references/nextjs/auth-middleware
   // for more information about configuring your Middleware
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
