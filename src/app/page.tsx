a2aea6ce4fbd7e334e849095657aa0b4b3a77b72
Tommy Grabowski <tommy.grabowski@velocitor.com>
2024-03-06 11:24:16 -0500

    Updated user email env variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,9 @@ export default async function Home() {
         `}
       >
         <QRCode />
-        {qrCodes && userEmail === process.env.WRITER_EMAIL_2 ? (
+        {qrCodes &&
+        (userEmail === process.env.WRITER_EMAIL_1 ||
+          userEmail === process.env.WRITER_EMAIL_2) ? (
           <ChangeQRCodeLinkForm qrCodes={qrCodes} />
         ) : null}
       </div>
