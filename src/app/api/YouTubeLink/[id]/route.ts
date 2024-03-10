fea1eabf073706a71fa35a9ad47bfcd4409c2bb0
Tommy Boi <tomaarongrabowski@gmail.com>
2024-03-10 14:21:09 -0400

    Updated API route

--- src/app/api/YouTubeLink/[id]/route.ts
+++ src/app/api/YouTubeLink/[id]/route.ts
@@ -1,9 +1,19 @@
+import { readQRCode } from '@/db/prisma';
 import { redirect } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
+  // Get the ID from the URL
   const lastCharacterOfURL = request.url[request.url.length - 1];
-  console.log('request', lastCharacterOfURL);
-  return redirect('https://www.youtube.com/watch?v=8');
+  // Get the QR Code from the database
+  const qrCode = await readQRCode(Number(lastCharacterOfURL));
+
+  if (!qrCode || !qrCode.youtube_url) {
+    // If the QR Code does not exist, redirect to the home page
+    return redirect('/');
+  }
+
+  // Redirect to the YouTube URL
+  return redirect(qrCode.youtube_url);
 }
