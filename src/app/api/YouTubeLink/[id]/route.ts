d2f883784796d102c1ff46acba6e1b07d9a3ac3b
Tommy Grabowski <tommy.grabowski@velocitor.com>
2024-03-12 14:51:25 -0400

    Updated route handler

--- src/app/api/YouTubeLink/[id]/route.ts
+++ src/app/api/YouTubeLink/[id]/route.ts
@@ -1,20 +1,41 @@
 import { readQRCode } from '@/db/prisma';
 import { redirect } from 'next/navigation';
+import { NextRequest, NextResponse } from 'next/server';
 
+// This is a dynamic route
 export const dynamic = 'force-dynamic';
 
-export async function GET(request: Request) {
-  // Get the ID from the URL
-  const lastCharacterOfURL = request.url[request.url.length - 1];
+type YouTubeLinkRouteParams = {
+  request: NextRequest;
+  response: NextResponse;
+  params: {
+    id: string;
+  };
+};
 
-  // Get the QR Code from the database
-  const qrCode = await readQRCode(Number(lastCharacterOfURL));
+export async function GET({
+  params,
+  response,
+  request,
+}: YouTubeLinkRouteParams) {
+  // Get the request ID
+  const id = params.id;
 
-  if (!qrCode || !qrCode.youtube_url) {
-    // If the QR Code does not exist, redirect to the home page
-    return redirect('https://www.youtube.com/watch?v=vh1I97pXuRw');
+  // Check if the ID is a number and if it is greater than 0
+  if (isNaN(Number(id)) || Number(id) <= 0) {
+    // If the ID is not a number or is less than or equal to 0, redirect to the home page
+    return redirect('/');
   }
 
-  // Redirect to the YouTube URL
+  // Get the QR Code from the database
+  const qrCode = await readQRCode(Number(id));
+
+  // Check if the QR Code exists or if it has a YouTube URL
+  if (!qrCode || !qrCode.youtube_url) {
+    // If the QR Code does not exist or does not have a YouTube URL, redirect to the home page
+    return redirect('/');
+  }
+
+  // If all checks pass, redirect to the YouTube URL
   return redirect(qrCode.youtube_url);
 }
