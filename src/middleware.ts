4e8885e765d1643963b288ec1ea7723cc5cb810b
Tommy Boi <tomaarongrabowski@gmail.com>
2024-03-11 20:51:06 -0400

    Updated Middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,8 @@ export default authMiddleware({
   // This pattern now matches /api/youtube/ followed by any characters
   publicRoutes: [
     '/',
+    '/QRCode',
+    /^\/QRCode\/([^\/]+)$/,
     '/api',
     /^\/api\/([^\/]+)$/,
     /^\/api\/YouTubeLink\/([^\/]+)$/,
